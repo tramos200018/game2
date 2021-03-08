e1eea908ed2cad15bf4d95062034ea88e3c0e978
AbdulBasit Ajeigbe <abdulbasitajeigbe@AbdulBasits-MBP.fios-router.home>
2021-03-07 21:57:23 -0500

    level movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use crate::types::{Rect, Rgba, Vec2i};
 use pixels::{Pixels, SurfaceTexture};
-use std::path::Path;
+use std::{borrow::Borrow, os::macos::raw::stat, path::Path, task::RawWakerVTable};
 use std::rc::Rc;
 use std::time::Instant;
-use winit::dpi::LogicalSize;
+use winit::{dpi::LogicalSize, event};
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
@@ -34,27 +34,262 @@ mod types;
 use types::*;
 
 mod collision;
+use collision::{Mobile, Wall, rect_touching};
+type Color = [u8; DEPTH];
 
+const CLEAR_COL: Color = [32, 32, 64, 255];
+const WALL_COL: Color = [200, 200, 200, 255];
+const PLAYER_COL: Color = [255, 128, 128, 255];
+const NEXT_COL: Color = [255, 0 , 0, 255];
+
+struct Level {gamemap: Vec<Wall>, exit: collision::Rect, position: Vec2i}
 
 // Now this main module is just for the run-loop and rules processing.
 struct GameState {
     // What data do we need for this game?  Wall positions?
     // Colliders?  Sprites and stuff?
-    animations: Vec<Animation>,
-    textures: Vec<Rc<Texture>>,
-    sprites: Vec<Sprite>,
-    maps: Vec<Tilemap>,
-    scroll: Vec2i,
+    player: Mobile,
+    //animations: Vec<Animation>,
+    //textures: Vec<Rc<Texture>>,
+    //sprites: Vec<Sprite>,
+    //maps: Vec<Tilemap>,
+    //scroll: Vec2i,
     levels: Vec<Level>,
+    current_level: usize,
 }
 // seconds per frame
 const DT: f64 = 1.0 / 60.0;
 
-const WIDTH: usize = 128;
-const HEIGHT: usize = 128;
+const WIDTH: usize = 700;
+const HEIGHT: usize = 550;
 const DEPTH: usize = 4;
 
 fn main() {
+    let walls1: Vec<Wall> = vec![
+        //top wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: WIDTH as u16,
+                h: 100,
+            },
+        },
+        //left wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: 150,
+                h: HEIGHT as u16,
+            },
+        },
+        //right wall
+        Wall {
+            rect: collision::Rect {
+                x: WIDTH as i32 / 3 * 2,
+                y: 0,
+                w: WIDTH as u16 / 3,
+                h: HEIGHT as u16,
+            },
+        },
+        //bottom wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: HEIGHT as i32 - 16,
+                w: WIDTH as u16,
+                h: 16,
+            },
+        },
+        //square wall
+        Wall {
+            rect: collision::Rect {
+                x: WIDTH as i32 / 2,
+                y: HEIGHT as i32 / 2,
+                w: 150,
+                h: 300,
+            },
+        },
+    ];
+    let walls2: Vec<Wall> = vec![
+        //top wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: WIDTH as u16,
+                h: 0,
+            },
+        },
+        //left wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: 90,
+                h: HEIGHT as u16,
+            },
+        },
+        //right wall
+        Wall {
+            rect: collision::Rect {
+                x: WIDTH as i32 - 26,
+                y: 0,
+                w: 90,
+                h: HEIGHT as u16,
+            },
+        },
+        //bottom wall 
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: HEIGHT as i32 - 30,
+                w: WIDTH as u16,
+                h: 70,
+            },
+        },
+        //first quarter wall
+        Wall {
+            rect: collision::Rect {
+                x: 220,
+                y: 90,
+                w: WIDTH as u16,
+                h: 70,
+            },
+        },
+        //second quarter wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 240,
+                w: WIDTH as u16 - 90,
+                h: 70,
+            },
+        },
+        //third quarter wall
+        Wall {
+            rect: collision::Rect {
+                x: 220,
+                y: 390,
+                w: WIDTH as u16,
+                h: 70,
+            },
+        },
+        
+    ];
+    let walls3: Vec<Wall> = vec![
+        //bottom wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: HEIGHT as i32 - 50,
+                w: WIDTH as u16,
+                h: 50,
+            },
+        },
+        //right wall
+        Wall {
+            rect: collision::Rect {
+                x: WIDTH as i32 - 150,
+                y: 0,
+                w: 150,
+                h: HEIGHT as u16,
+            },
+        },
+        //left wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: 100,
+                h: HEIGHT as u16,
+            },
+        },
+        //top wall
+        Wall {
+            rect: collision::Rect {
+                x: 0,
+                y: 0,
+                w: WIDTH as u16,
+                h: 50,
+            },
+        },
+        //w1
+        Wall {
+            rect: collision::Rect {
+                x: 100,
+                y: HEIGHT as i32 - 150,
+                w: WIDTH as u16 / 3 + 150,
+                h: 50,
+            },
+        },
+        //w2
+        Wall {
+            rect: collision::Rect {
+                x: 100 + 50,
+                y: HEIGHT as i32 - 350,
+                w: WIDTH as u16 / 3 + 200,
+                h: 150,
+            },
+        },
+        //w3
+        Wall {
+            rect: collision::Rect {
+                x: 100,
+                y: 50,
+                w: WIDTH as u16 / 3,
+                h: 100,
+            },
+        },
+        //w4
+        Wall {
+            rect: collision::Rect {
+                x: 100 + WIDTH as i32 / 3,
+                y: HEIGHT as i32 - 375,
+                w: WIDTH as u16 / 3 + 100,
+                h: 25,
+            },
+        },
+        //w5
+        Wall {
+            rect: collision::Rect {
+                x: WIDTH as i32 / 3 * 2 - 50,
+                y: 50,
+                w: WIDTH as u16 / 3 + 50,
+                h: 150,
+            },
+        },
+        //w6
+        Wall {
+            rect: collision::Rect {
+                x: 100 + WIDTH as i32 / 3,
+                y: 125,
+                w: 50,
+                h: 25,
+            },
+        },
+        //w7
+        Wall {
+            rect: collision::Rect {
+                x: 130 + WIDTH as i32 / 3,
+                y: 93,
+                w: 60,
+                h: 3,
+            },
+        },
+        //w8
+        Wall {
+            rect: collision::Rect {
+                x: 100 + WIDTH as i32 / 3,
+                y: 50,
+                w: 40,
+                h: 15,
+            },
+        },
+        
+    ];
+    /* 
     let tex = Rc::new(Texture::with_file(Path::new("pixil-frame-0.png")));
     let tileset = Rc::new(Tileset::new(
         vec![Tile { solid: true }, Tile { solid: true }],
@@ -104,7 +339,7 @@ fn main() {
     maps.push(map);
     maps.push(map2);
     maps.push(map3);
-    maps.push(map4);
+    maps.push(map4);*/
 
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
@@ -123,6 +358,7 @@ fn main() {
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
         Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture).unwrap()
     };
+    /* 
     let tex = Rc::new(Texture::with_file(Path::new("king.png")));
     let frame1 = Rect {
         x: 0,
@@ -137,25 +373,62 @@ fn main() {
         h: 16,
     };
     let mut anim = Rc::new(Animation::new(vec![frame1, frame2]));
-    let mut scroll = Vec2i(0, 0);
+    let mut scroll = Vec2i(0, 0);*/
+    /*let player = Mobile {
+        rect: collision::Rect {
+            x: 170,
+            y: 170,
+            w: 16,
+            h: 16,
+        },
+        vx: 0,
+        vy: 0,
+    };*/
+    //let game_walls: Vec<Wall> = [walls1, walls2, walls3];
+    let level =  Level{
+        gamemap: walls1,
+        exit: collision::Rect {
+            x: WIDTH as i32 / 2 + 50,
+            y: 100,
+            w: 68,
+            h: 175,},
+        position: Vec2i(170, 170)
+    };
+    let level2 =  Level{
+        gamemap: walls2,
+        exit: collision::Rect {
+            x: WIDTH as i32 - 50,
+            y: 460,
+            w: 30,
+            h: 60,},
+        position: Vec2i(WIDTH as i32 - 55, 15)
+    };
+    let level3 =  Level{
+        gamemap: walls3,
+        //need to correct exit
+        exit: collision::Rect {
+            x: WIDTH as i32 - 50,
+            y: 460,
+            w: 30,
+            h: 60,},
+        position: Vec2i(110, 463)
+    };
+
+
     let mut state = GameState {
         // initial game state...
-        animations: vec![],
-        sprites: vec![Sprite::new(
-            &tex,
-            &anim,
-            Rect {
-                x: 0,
-                y: 16,
+        player:  Mobile {
+            rect: collision::Rect {
+                x: 170,
+                y: 500,
                 w: 16,
                 h: 16,
             },
-            0,
-            Vec2i(0, 0),
-        )],
-        textures: vec![tex],
-        maps,
-        scroll,
+            vx: 0,
+            vy: 0,
+        },
+        levels: vec![level, level2, level3],
+        current_level: 0,
     };
     // How many frames have we simulated?
     let mut frame_count: usize = 0;
@@ -168,10 +441,18 @@ fn main() {
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
-            let mut screen = Screen::wrap(pixels.get_frame(), WIDTH, HEIGHT, DEPTH, Vec2i(0, 0));
-            screen.clear(Rgba(0, 0, 0, 0));
+            let fb = pixels.get_frame();
+            collision::clear(fb, CLEAR_COL);
 
-            draw_game(&mut state, &mut screen);
+            //Draw the walls
+            for w in state.levels[state.current_level].gamemap.iter() {
+                collision::rect(fb, w.rect, WALL_COL);
+            }
+            //draw the exit
+            collision::rect(fb, state.levels[state.current_level].exit, NEXT_COL);
+            // Draw the player
+            collision::rect(fb, state.player.rect, PLAYER_COL);
+            //draw_game(&mut state, fb);
 
             // Flip buffers
             if pixels.render().is_err() {
@@ -212,60 +493,56 @@ fn main() {
         since = Instant::now();
     });
 }
-
-fn draw_game(state: &mut GameState, screen: &mut Screen) {
+/* 
+fn draw_game(state: &mut GameState, fb: &mut u8) {
     // Call screen's drawing methods to render the game state
-    screen.clear(Rgba(80, 80, 80, 255));
+    collision::clear(fb, CLEAR_COL);
+    
 
-    for map in state.maps.iter() {
-        map.draw(screen);
-    }
-
-    for s in state.sprites.iter() {
-        screen.draw_sprite(s);
+    for w in state.levels.gamemap[0].iter() {
+        collision::rect(fb, w.rect, WALL_COL);
     }
 
-    if state.sprites[0].position.0 > WIDTH as i32 && state.sprites[0].position.0 < WIDTH as i32 * 2
-    {
-        screen.clear(Rgba(80, 80, 80, 255));
-        screen.position = state.maps[1].position;
-    }
-    if state.sprites[0].position.0 > WIDTH as i32 * 2
-        && state.sprites[0].position.0 < WIDTH as i32 * 3
-    {
-        screen.clear(Rgba(80, 80, 80, 255));
-        screen.position = state.maps[2].position;
-    }
-    if state.sprites[0].position.0 > WIDTH as i32 * 3 {
-        screen.clear(Rgba(80, 80, 80, 255));
-        screen.position = state.maps[3].position;
-    }
-    for map in state.maps.iter() {
-        map.draw(screen);
-    }
-
-    for s in state.sprites.iter() {
-        screen.draw_sprite(s);
-    }
-}
+    
+}*/
 
 fn update_game(state: &mut GameState, input: &WinitInputHelper, frame: usize) {
+    let mut level_index: usize = state.current_level;
     // Player control goes here
     if input.key_held(VirtualKeyCode::Right) {
-        state.sprites[0].position.0 += 2;
+        state.player.rect.x += 1;
     }
     if input.key_held(VirtualKeyCode::Left) {
-        state.sprites[0].position.0 -= 2;
+        state.player.rect.x -= 1;
     }
     if input.key_held(VirtualKeyCode::Up) {
-        state.sprites[0].position.1 -= 2;
+        state.player.rect.y -= 1;
     }
     if input.key_held(VirtualKeyCode::Down) {
-        state.sprites[0].position.1 += 2;
+        state.player.rect.y += 1;
     }
     // Update player position
 
     // Detect collisions: Generate contacts
+    /*for w in state.levels[state.current_level].gamemap.iter() {
+        if collision::rect_touching(state.player.rect, w.rect){
+            level_index = 0;
+            state.current_level = level_index;
+            state.player.rect.x = 170;
+            state.player.rect.y = 500;
+            break;
+        }
+    }*/
+
+    if collision::rect_touching(state.player.rect, state.levels[state.current_level].exit){
+        //change level here
+        level_index += 1;
+        state.current_level = level_index;
+        state.player.rect.x = state.levels[state.current_level].position.0;
+        state.player.rect.y = state.levels[state.current_level].position.1;
+
+    }
+    
 
     // Handle collisions: Apply restitution impulses.
 
