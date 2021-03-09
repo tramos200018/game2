31f423eade13ba96f3debb4f97983dde18445f4c
Antonio Ramos <antonioramos@Antonios-Air.attlocal.net>
2021-03-08 19:57:32 -0600

    press SPACE to start

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use crate::types::{Rect, Rgba, Vec2i};
 use pixels::{Pixels, SurfaceTexture};
-use std::{borrow::Borrow, os::macos::raw::stat, path::Path, task::RawWakerVTable};
 use std::rc::Rc;
 use std::time::Instant;
-use winit::{dpi::LogicalSize, event};
+use std::{borrow::Borrow, os::macos::raw::stat, path::Path, task::RawWakerVTable};
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit::{dpi::LogicalSize, event};
 use winit_input_helper::WinitInputHelper;
 
 // Whoa what's this?
@@ -34,15 +34,26 @@ mod types;
 use types::*;
 
 mod collision;
-use collision::{Mobile, Wall, rect_touching};
+use collision::{rect_touching, Mobile, Wall};
 type Color = [u8; DEPTH];
 
 const CLEAR_COL: Color = [32, 32, 64, 255];
 const WALL_COL: Color = [200, 200, 200, 255];
 const PLAYER_COL: Color = [255, 128, 128, 255];
-const NEXT_COL: Color = [255, 0 , 0, 255];
+const NEXT_COL: Color = [255, 0, 0, 255];
 
-struct Level {gamemap: Vec<Wall>, exit: collision::Rect, position: Vec2i}
+struct Level {
+    gamemap: Vec<Wall>,
+    exit: collision::Rect,
+    position: Vec2i,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Mode {
+    TitleScreen,
+    GamePlay,
+    EndGame,
+}
 
 // Now this main module is just for the run-loop and rules processing.
 struct GameState {
@@ -50,12 +61,13 @@ struct GameState {
     // Colliders?  Sprites and stuff?
     player: Mobile,
     //animations: Vec<Animation>,
-    //textures: Vec<Rc<Texture>>,
+    textures: Vec<Rc<Texture>>,
     //sprites: Vec<Sprite>,
     //maps: Vec<Tilemap>,
     //scroll: Vec2i,
     levels: Vec<Level>,
     current_level: usize,
+    mode: Mode,
 }
 // seconds per frame
 const DT: f64 = 1.0 / 60.0;
@@ -65,6 +77,13 @@ const HEIGHT: usize = 550;
 const DEPTH: usize = 4;
 
 fn main() {
+    //let player_tex = Rc::new(Texture::with_file(Path::new("king.png")));
+
+    //let enemy_tex = Rc::clone(&player_tex);
+
+    let startscreen_tex = Rc::new(Texture::with_file(Path::new("start.png")));
+    let endscreen_tex = Rc::new(Texture::with_file(Path::new("end.jpg")));
+
     let walls1: Vec<Wall> = vec![
         //top wall
         Wall {
@@ -140,7 +159,7 @@ fn main() {
                 h: HEIGHT as u16,
             },
         },
-        //bottom wall 
+        //bottom wall
         Wall {
             rect: collision::Rect {
                 x: 0,
@@ -176,7 +195,6 @@ fn main() {
                 h: 70,
             },
         },
-        
     ];
     let walls3: Vec<Wall> = vec![
         //bottom wall
@@ -287,7 +305,6 @@ fn main() {
                 h: 15,
             },
         },
-        
     ];
 
     let event_loop = EventLoop::new();
@@ -307,40 +324,42 @@ fn main() {
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
         Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture).unwrap()
     };
-   
-    let level =  Level{
+
+    let level = Level {
         gamemap: walls1,
         exit: collision::Rect {
             x: WIDTH as i32 / 2 + 50,
             y: 100,
             w: 68,
-            h: 175,},
-        position: Vec2i(170, 170)
+            h: 175,
+        },
+        position: Vec2i(170, 170),
     };
-    let level2 =  Level{
+    let level2 = Level {
         gamemap: walls2,
         exit: collision::Rect {
             x: WIDTH as i32 - 50,
             y: 460,
             w: 30,
-            h: 60,},
-        position: Vec2i(WIDTH as i32 - 55, 15)
+            h: 60,
+        },
+        position: Vec2i(WIDTH as i32 - 55, 15),
     };
-    let level3 =  Level{
+    let level3 = Level {
         gamemap: walls3,
         //need to correct exit
         exit: collision::Rect {
             x: 373,
             y: 50,
             w: 43,
-            h: 10,},
-        position: Vec2i(110, 463)
+            h: 10,
+        },
+        position: Vec2i(110, 463),
     };
-
 
     let mut state = GameState {
         // initial game state...
-        player:  Mobile {
+        player: Mobile {
             rect: collision::Rect {
                 x: 170,
                 y: 500,
@@ -350,8 +369,11 @@ fn main() {
             vx: 0,
             vy: 0,
         },
-        levels: vec![level3, level, level2],
+        textures: vec![Rc::clone(&startscreen_tex), Rc::clone(&endscreen_tex)],
+        levels: vec![level, level2, level3],
         current_level: 0,
+        // Current mode
+        mode: Mode::TitleScreen,
     };
     // How many frames have we simulated?
     let mut frame_count: usize = 0;
@@ -361,6 +383,7 @@ fn main() {
     let start = Instant::now();
     // Track end of the last frame
     let mut since = Instant::now();
+    println!("Press Space to start!");
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
@@ -375,6 +398,7 @@ fn main() {
             collision::rect(fb, state.levels[state.current_level].exit, NEXT_COL);
             // Draw the player
             collision::rect(fb, state.player.rect, PLAYER_COL);
+            
             //draw_game(&mut state, fb);
 
             // Flip buffers
@@ -387,6 +411,8 @@ fn main() {
             // The renderer "produces" time...
             available_time += since.elapsed().as_secs_f64();
         }
+        
+
         // Handle input events
         if input.update(event) {
             // Close events
@@ -402,10 +428,12 @@ fn main() {
         // And the simulation "consumes" it
         while available_time >= DT {
             let mut screen = Screen::wrap(pixels.get_frame(), WIDTH, HEIGHT, DEPTH, Vec2i(0, 0));
+
+
             // Eat up one frame worth of time
             available_time -= DT;
 
-            update_game(&mut state, &input, frame_count);
+            update_game(&mut state, &input, frame_count, &mut screen);
 
             // Increment the frame counter
             frame_count += 1;
@@ -417,46 +445,110 @@ fn main() {
     });
 }
 
+fn start_game(state: &mut GameState, input: &WinitInputHelper, frame: usize, screen: &mut Screen) {
+    screen.clear(Rgba(80, 80, 80, 255));
 
-fn update_game(state: &mut GameState, input: &WinitInputHelper, frame: usize) {
-    let mut level_index: usize = state.current_level;
-    // Player control goes here
-    if input.key_held(VirtualKeyCode::Right) {
-        state.player.rect.x += 1;
+    match state.mode {
+        Mode::TitleScreen => screen.bitblt(
+            &state.textures[1],
+            Rect {
+                x: 0,
+                y: 0,
+                w: 200,
+                h: 200,
+            },
+            Vec2i(0, 0),
+        ),
+        Mode::GamePlay => {}
+        Mode::EndGame => screen.bitblt(
+            &state.textures[1],
+            Rect {
+                x: 0,
+                y: 0,
+                w: 200,
+                h: 200,
+            },
+            Vec2i(0, 0),
+        ),
     }
-    if input.key_held(VirtualKeyCode::Left) {
-        state.player.rect.x -= 1;
-    }
-    if input.key_held(VirtualKeyCode::Up) {
-        state.player.rect.y -= 1;
-    }
-    if input.key_held(VirtualKeyCode::Down) {
-        state.player.rect.y += 1;
-    }
-    // Update player position
+}
 
-    // Detect collisions: Generate contacts
-    for w in state.levels[state.current_level].gamemap.iter() {
-        if collision::rect_touching(state.player.rect, w.rect){
-            level_index = 0;
-            state.current_level = level_index;
-            state.player.rect.x = 170;
-            state.player.rect.y = 500;
-            break;
+//maybe add start game for menus
+fn update_game(state: &mut GameState, input: &WinitInputHelper, frame: usize, screen: &mut Screen) {
+    {
+        match state.mode {
+            Mode::TitleScreen => {
+                if input.key_held(VirtualKeyCode::Space) {
+                    state.mode = Mode::GamePlay
+                }
+                screen.bitblt(
+                    &state.textures[1],
+                    Rect {
+                        x: 0,
+                        y: 0,
+                        w: 50,
+                        h: 50,
+                    },
+                    Vec2i(0, 0),
+                )
+            }
+            Mode::GamePlay => {
+                let mut level_index: usize = state.current_level;
+                // Player control goes here
+                if input.key_held(VirtualKeyCode::Right) {
+                    state.player.rect.x += 1;
+                }
+                if input.key_held(VirtualKeyCode::Left) {
+                    state.player.rect.x -= 1;
+                }
+                if input.key_held(VirtualKeyCode::Up) {
+                    state.player.rect.y -= 1;
+                }
+                if input.key_held(VirtualKeyCode::Down) {
+                    state.player.rect.y += 1;
+                }
+                // Update player position
+
+                // Detect collisions: Generate contacts
+                for w in state.levels[state.current_level].gamemap.iter() {
+                    if collision::rect_touching(state.player.rect, w.rect) {
+                        level_index = 0;
+                        state.current_level = level_index;
+                        state.player.rect.x = 170;
+                        state.player.rect.y = 500;
+                        break;
+                    }
+                }
+
+                if collision::rect_touching(
+                    state.player.rect,
+                    state.levels[state.current_level].exit,
+                ) {
+                    //change level here
+                    level_index += 1;
+                    state.current_level = level_index;
+                    state.player.rect.x = state.levels[state.current_level].position.0;
+                    state.player.rect.y = state.levels[state.current_level].position.1;
+                }
+            }
+            Mode::EndGame => {
+                if input.key_held(VirtualKeyCode::P) {
+                    screen.bitblt(
+                        &state.textures[1],
+                        Rect {
+                            x: 0,
+                            y: 0,
+                            w: 100,
+                            h: 100,
+                        },
+                        Vec2i(0, 0),
+                    )
+                }
+            }
         }
+
+        // Handle collisions: Apply restitution impulses.
+
+        // Update game rules: What happens when the player touches things?
     }
-
-    if collision::rect_touching(state.player.rect, state.levels[state.current_level].exit){
-        //change level here
-        level_index += 1;
-        state.current_level = level_index;
-        state.player.rect.x = state.levels[state.current_level].position.0;
-        state.player.rect.y = state.levels[state.current_level].position.1;
-
-    }
-    
-
-    // Handle collisions: Apply restitution impulses.
-
-    // Update game rules: What happens when the player touches things?
 }
